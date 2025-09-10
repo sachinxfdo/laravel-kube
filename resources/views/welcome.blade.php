88d87d2ff2264a58234508463829c7507c0247c7
sachinxfdo <madsachintha1234@gmail.com>
2025-09-10 22:37:34 +0530

    test2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -73,11 +73,11 @@
                                         xmlns="http://www.w3.org/2000/svg"
                                         class="w-2.5 h-2.5"
                                     >
-                                        <path
+                                        {{-- <path
                                             d="M7.70833 6.95834V2.79167H3.54167M2.5 8L7.5 3.00001"
                                             stroke="currentColor"
                                             stroke-linecap="square"
-                                        />
+                                        /> --}}
                                     </svg>
                                 </a>
                             </span>
